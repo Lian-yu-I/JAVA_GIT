c7e141ae4ff7e3463f4e07b34e4dd7def0fc24ab
lianyu <1664516859@qq.com>
2021-12-20 09:15:27 +0800

    Git_Idea_test_master_git_test3_commit

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.println("push git");
         System.out.println("push git");
         System.out.println("push git");
-
+        System.out.println("ssh push");
     }
 }
